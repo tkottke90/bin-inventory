ae2300ec971bb3151d6f98f4f0c443afa43acde1
Thomas Kottke <t.kottke90@gmail.com>
2020-09-24 21:04:19 -0500

    refactor: Add data services

---
+++ src/ui/src/app/services/container.service.ts
@@ -0,0 +1,18 @@
+import { HTTPService } from './http.service';
+
+
+export class ContainerServuce {
+  public static find() {}
+
+  public static get(id: string) {}
+
+  public static create() {}
+
+  public static createOrUpdate() {}
+
+  public static update() {}
+
+  public static delete() {}
+
+  private baseUrl = '/api/containers'
+}
