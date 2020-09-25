89c4880b715ab16d5e573edf457735dc5aeec593
Thomas Kottke <t.kottke90@gmail.com>
2020-09-24 21:04:19 -0500

    refactor: Add data services

---
+++ src/ui/src/app/services/user.service.ts
@@ -0,0 +1,41 @@
+import { BehaviorSubject } from 'rxjs';
+import { HTTPService } from './http.service';
+
+export interface IUser {
+  readonly id: number;
+  firstName: string;
+  lastName: string;
+  readonly email: string;
+  password?: string;
+  type: string;
+  active: boolean;
+  settings?: any;
+  auth?: any;
+  readonly createdAt: Date;
+  readonly updatedAt: Date;
+}
+
+export class UserService {
+  
+  public static $user: BehaviorSubject<IUser | false> = new BehaviorSubject<IUser | false>(false);
+
+  public static $getCurrentUser(): IUser | false {
+    return this.$user.value;
+  }
+
+  public static find(query: string) {
+
+  }
+
+  public static get(id: number) {}
+
+  public static create(user: IUser) {}
+
+  public static createOrUpdate(id: number, user: IUser) {}
+
+  public static update(id: number, user: Partial<IUser>) {}
+
+  public static delete(id: number) {}
+
+  private baseUrl = '/api/users'
+}
