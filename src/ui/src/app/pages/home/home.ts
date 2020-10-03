0ed228537d8ffae0b4b24372eeb01717f6b8a2f8
Thomas Kottke <t.kottke90@gmail.com>
2020-10-03 13:51:06 -0500

    feat(home): Add app shell to home page

--- src/ui/src/app/pages/home/home.ts
+++ src/ui/src/app/pages/home/home.ts
@@ -8,12 +8,8 @@ const tag = 'home-page';
 class HomePage extends PageComponent {
   render() {
     return html`
-      <style></style>
-      <h1 class=${styles.header}>CRA Project Framework</h1>
-      <main>
-        <h4>Welcome to the CRA Framework,</h4>
-        <p><strong>Tools:</strong> Typescript, PageJS, RollupJS</p>
-      </main>
+      <app-shell title="Bin Inventory">
+      </app-shell>
     `
   }
 }
